9673eb77d76b473a52082e3c959c3462d08c5644
MEBARKI16 <medaminemebarki@gmail.com>
2025-01-06 09:37:04 +0100

    understanding Requests

--- UnderstandingTheBasics/app.js
+++ UnderstandingTheBasics/app.js
@@ -2,9 +2,7 @@ const http = require('http');
 
 // Créer un serveur HTTP
 http.createServer((req, res) => {
-    console.log("hello"); // Affiche "hello" dans la console à chaque requête
-    res.writeHead(200, { 'Content-Type': 'text/plain' }); // Définir le statut et le type de contenu
-    res.end('Hello, world!'); // Envoyer une réponse au client
+    console.log(req.url, req.method, req.headers); // Affiche "hello" dans la console à chaque requête
 }).listen(3000, () => {
     console.log('Serveur démarré sur le port 3000'); // Indiquer que le serveur est prêt
 });
