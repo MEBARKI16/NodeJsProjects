5761ee38fdeb84ebd6dc283f56f8cf7bf1a3e510
MEBARKI16 <medaminemebarki@gmail.com>
2025-01-13 10:19:18 +0100

    Routing Requests

--- UnderstandingTheBasics/app.js
+++ UnderstandingTheBasics/app.js
@@ -1,16 +1,34 @@
 const http = require('http');
 
 http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+    const url = req.url;
+    if (url === '/') {
+        res.write(`
+        <html>
+            <head>
+                <title>Enter Message</title>
+            </head>
+            <body>
+                <form action="/message" method="POST">
+                    <input type="text" name="message" />
+                    <button type="submit">soummetre</button>
+                </form>
+            </body>
+        </html>
+        `);
+        return res.end();
+    }
     res.setHeader('Content-Type', 'text/html');
-    res.write(`<html>
-        <head>
-        <title>Mon Serveur Node JS</title>
-        </head>
-        <body> 
-         <p style="color:red">hello world</p>
-        </body>
-      </html>`)
+    res.write(`
+        <html>
+            <head>
+                <title>My First Page</title>
+            </head>
+            <body>
+                <h1>Hello from my Node js Server!</h1>
+            </body>
+        </html>
+    `);
     res.end();
 }).listen(3000, () => {
     console.log('Serveur démarré sur le port 3000');
