f0d044630bc2dd85a5992bedf4c2b2f5bae64034
MEBARKI16 <medaminemebarki@gmail.com>
2025-02-02 14:26:05 +0100

    Using Express Router

---
+++ WorkingWithExpressJs/routes/admin.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+
+router.use(express.urlencoded({ extended: true }));
+router.get("/add-product", (req, res) => {
+  res.send(`
+      <form action="/product" method="POST">
+      <label for="name">name</label>
+      <input type="text" name="name" id="name" />
+      <button type="submit">ok</button>
+      </form>
+      `);
+});
+
+router.post("/product", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+module.exports = router;
--- WorkingWithExpressJs/server.js
+++ WorkingWithExpressJs/server.js
@@ -1,20 +1,10 @@
 const express = require("express");
 const app = express();
-app.use(express.urlencoded({ extended: true }));
-app.get("/", (req, res) => {
-  res.send(`
-      <form action="/afficheName" method="POST">
-      <label for="name">name</label>
-      <input type="text" name="name" id="name" />
-      <button type="submit">ok</button>
-      </form>
-      `);
-});
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.post("/afficheName", (req, res) => {
-  console.log(req.body);
-  res.redirect("/");
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000, () => {
   console.log("serveur démarré sur localhost:3000");
