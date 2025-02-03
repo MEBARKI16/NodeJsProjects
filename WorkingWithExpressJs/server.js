bce3e3cc80e57e4088084f8c51879569ba76a93e
MEBARKI16 <medaminemebarki@gmail.com>
2025-02-03 10:04:25 +0100

    Adding a 404 Error Page

--- WorkingWithExpressJs/server.js
+++ WorkingWithExpressJs/server.js
@@ -5,7 +5,9 @@ const shopRoutes = require("./routes/shop");
 
 app.use(adminRoutes);
 app.use(shopRoutes);
-
+app.use((req,res,next) => {
+  res.status(404).send('<h1>Page Not Found</h1>')
+})
 app.listen(3000, () => {
   console.log("serveur démarré sur localhost:3000");
 });
