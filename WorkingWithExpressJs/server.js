620dda0805dacfb74bc6adda8fd50662561b5bc1
Mohamed Amine MEBARKI <mohamed.mebarki@UNODIS.DZ>
2025-02-17 14:24:22 +0100

    Styling our pages

--- WorkingWithExpressJs/server.js
+++ WorkingWithExpressJs/server.js
@@ -4,6 +4,7 @@ const path = require("path");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use((req, res, next) => {
