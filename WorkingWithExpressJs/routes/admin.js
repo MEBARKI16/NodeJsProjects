4c96ece3b09a06ee4e4fcb3fbf7dae8c46c5b333
Mohamed Amine MEBARKI <mohamed.mebarki@UNODIS.DZ>
2025-02-18 15:21:19 +0100

    Sharing Data Across Requests & Users

--- WorkingWithExpressJs/routes/admin.js
+++ WorkingWithExpressJs/routes/admin.js
@@ -1,15 +1,21 @@
-const express = require("express");
 const path = require("path");
-const router = express.Router();
 
-router.use(express.urlencoded({ extended: true }));
-router.get("/add-product", (req, res) => {
-  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+const express = require("express");
+
+const rootDir = require("../util/path");
+
+const router = express.Router();
+const products = [];
+// /admin/add-product => GET
+router.get("/add-product", (req, res, next) => {
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
-router.post("/add-product", (req, res) => {
-  console.log(req.body);
+// /admin/add-product => POST
+router.post("/add-product", (req, res, next) => {
+  products.push({ title: req.body.title });
   res.redirect("/");
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
